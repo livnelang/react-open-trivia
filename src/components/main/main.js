56698c0ac540651955d2421246a5523121e84354
Livne Lang <livnelang@gmail.com>
2019-07-28 19:42:29 +0300

    second commit

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,17 +1,27 @@
 import React, { PureComponent } from "react";
 import './main.css';
+import jsonData from './categories.json';
 
 
 export default class Main extends PureComponent {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: jsonData.categories
+        };
+    }
+
     render() {
         return (
-            //   <div className="topPanel flexColumnCenter">
-            //     <h1>Spot the genre!</h1>
-            //     <p>   Welcome aboard! Try to guess which genre this artist is playing!    </p>
-            //   </div>
-            null
+            <div>
+                {this.state.categories.map(function (item, index) {
+                    return <p key={index} value={item} />
+                }, this)
+                }
+            </div>
+            // null
         );
     }
 
-}
+}   
